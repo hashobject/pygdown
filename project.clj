e217a68d8f900f34ec3a80e7973013c22ac6d13a
Anton Podviaznikov <podviaznikov@gmail.com>
2013-06-28 23:35:19 -0600

    add aot section

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject pygdown "0.1.2"
+(defproject pygdown "0.1.3"
   :description "A Clojure library for markdown processing
   and html generating with syntax higlighting."
   :signing {:gpg-key "HashObject Ltd <team@hashobject.com>"}
   :url "https://github.com/hashobject/pygdown"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :aot [pygdown.serializer
+        pygdown.core]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.pegdown/pegdown "1.4.0"]
                  [pygmenter "0.1.1"]])
